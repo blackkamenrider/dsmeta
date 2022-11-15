8c8799db32fae9b2c9fe91a1de01f0f38463d1b5
blackkamenrider <hosias.souza@gmail.com>
2022-11-15 18:33:44 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return(
+   <>
     <h1>Iniciando a aplicaçao React e Spring Boot</h1>
+    <NotificationButton/>
+   </>
+      
+      
+    
+    
   )
 }
 
