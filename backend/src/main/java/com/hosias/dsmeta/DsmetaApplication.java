27b8f7f6b8f918146c8d56db7dbbf4687ca0da93
blackkamenrider <hosias.souza@gmail.com>
2022-11-18 10:58:23 -0300

    Security config

--- backend/src/main/java/com/hosias/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/hosias/dsmeta/DsmetaApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.hosias.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DsmetaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DsmetaApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/hosias/dsmeta/DsmetaApplicationTests.java
+++ backend/src/test/java/com/hosias/dsmeta/DsmetaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.hosias.dsmeta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DsmetaApplicationTests {
 
 	@Test
 	void contextLoads() {
