571fbe190378ec12fa5aa64f251bc830dc2433da
blackkamenrider <hosias.souza@gmail.com>
2022-11-21 10:29:35 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import NotificationButton from "./components/NotificationButton"
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
@@ -6,6 +7,8 @@ import SalesCard from "./components/SalesCard"
 function App() {
   return(
    <>
+   
+   <ToastContainer/>  {/* componente do react-toastify importao a cima. apartir desta declaraçao posso invocar uma mss em qualquer componente */}
     <Header/>
     <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 import './styles.css'
@@ -10,7 +11,7 @@ type Props = {
 {/*esta funçao a baixo e responsavel por chamar minha api no backend*/}
 function handleClick(id : number){
     axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-        console.log("sucesso");
+        toast.info("SMS enviado com Sucesso");
     }); {/*axios responsavel por fazer a requisiçao */}
 }
 
